7d2642603fa8fe8f45f677d1529dfbeb52198582
Ayfoth <147963605+Ayfoth@users.noreply.github.com>
2026-03-16 14:19:28 +0100

    Implantation du Biome Rubis

--- src/main/java/com/magius/world/mod/event/ModEventBusClientEvents.java
+++ src/main/java/com/magius/world/mod/event/ModEventBusClientEvents.java
@@ -6,34 +6,40 @@ import com.magius.world.mod.block.entity.renderer.FireFounderieBlockEntityRender
 import com.magius.world.mod.block.entity.renderer.GemPolishingBlockEntityRenderer;
 import com.magius.world.mod.entity.client.ModModelLayers;
 import com.magius.world.mod.entity.client.RhinoModel;
+import com.magius.world.mod.util.ModWoodTypes;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.ChestBoatModel;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
-import net.minecraft.world.entity.vehicle.Boat;
-import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import javax.swing.text.html.parser.Entity;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = MagiusWorldMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-
 public class ModEventBusClientEvents {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            Sheets.addWoodType(ModWoodTypes.PINE);
+            Sheets.addWoodType(ModWoodTypes.RUBY);
+        });
+    }
+
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
-
         event.registerLayerDefinition(ModModelLayers.PINE_BOAT_LAYER, BoatModel::createBodyModel);
         event.registerLayerDefinition(ModModelLayers.PINE_CHEST_BOAT_LAYER, ChestBoatModel::createBodyModel);
     }
+
     @SubscribeEvent
     public static void registerBER(EntityRenderersEvent.RegisterRenderers event){
         event.registerBlockEntityRenderer(ModBlockEntities.GEM_POLISHING_BE.get(), GemPolishingBlockEntityRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.FIRE_FOUNDERIE_BE.get(), FireFounderieBlockEntityRenderer::new);
-
         event.registerBlockEntityRenderer(ModBlockEntities.MOD_SIGN.get(), SignRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.MOD_HANGING_SIGN.get(), HangingSignRenderer::new);
     }
